14aa801c152cb0b2e15f66243e807205acc7c316
rahul <rahulp5875@gmail.com>
2024-06-06 12:09:11 +0530

    Add dependency for camerax.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.video)
 }
